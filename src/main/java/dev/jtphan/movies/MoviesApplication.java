c4e8f10dac4e644bd67c6b5bb8cd2547b887b1cc
Jonathan Phan <jonathan.tan.phan@gmail.com>
2023-09-29 12:29:11 -0700

    Initial Commit of Movies application

---
+++ src/main/java/dev/jtphan/movies/MoviesApplication.java
@@ -0,0 +1,22 @@
+package dev.jtphan.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController //States that this API is an REST API controller
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+	@GetMapping("/")
+	//GetMapping annotation lets framework know that it is a GET
+	//endpoint
+	public String apiRoot(){
+		return "Hello, World!";
+	}
+}
