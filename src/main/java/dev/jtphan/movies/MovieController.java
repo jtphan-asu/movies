2a0635c36d6afb8195f9721d335ba0235d20ad28
Jonathan Phan <jonathan.tan.phan@gmail.com>
2023-10-06 20:26:48 -0700

    Create functionality to use path variable as a way to look a singular movie instance from the mongoDB database

--- src/main/java/dev/jtphan/movies/MovieController.java
+++ src/main/java/dev/jtphan/movies/MovieController.java
@@ -1,13 +1,16 @@
 package dev.jtphan.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -20,4 +23,15 @@ public class MovieController {
         //Use MovieService to return all movies from MovieRepository. Then return '200' as HTTP status
        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
+
+    @GetMapping("/{id}")
+    //Lets Framework know that we are using Path Variable then converting to an ObjectId called id
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable ObjectId id) {
+
+        //Return a single movie instance using Path Variable and ObjectId
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(id), HttpStatus.OK);
+
+    }
+
+
 }
--- src/main/java/dev/jtphan/movies/MovieService.java
+++ src/main/java/dev/jtphan/movies/MovieService.java
@@ -1,9 +1,11 @@
 package dev.jtphan.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 //This is where most Business Logic will go
@@ -11,7 +13,13 @@ public class MovieService {
     @Autowired //Framework to auto instantiate this class
     private MovieRepository movieRepository; //Uses Repository class to talk to the database then returns to the API layer
 
+    //Return all movies from Database
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+
+    //Return a single movie based on ObjectId in database
+    public Optional<Movie> singleMovie(ObjectId id){
+        return movieRepository.findById(id);
     }
 }
