7cea4c8c0a6bd26de25f780a2a90bf36367e3789
Jonathan Phan <jonathan.tan.phan@gmail.com>
2023-10-24 07:50:50 -0700

    Create new review functionality and update MongoDB using payload from User

--- src/main/java/dev/jtphan/movies/ReviewController.java
+++ src/main/java/dev/jtphan/movies/ReviewController.java
@@ -1,6 +1,9 @@
 package dev.jtphan.movies;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +20,27 @@ public class ReviewController {
     private ReviewService reviewService;
 
     @PostMapping
+    //What we get as RequestBody we would like to convert into a Map of Key String and Value String called payload
     public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload){
+        //Receive JSON endpoint data from user then convert to a Map
         return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"),
                 payload.get("imdbId")), HttpStatus.CREATED);
+    }
+
+    private ReviewRepository reviewRepository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    //Create a new Review
+    public Review createReview(String reviewBody, String imdbId){
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        //Update MongoDB
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
     }
 }
--- src/main/java/dev/jtphan/movies/ReviewRepository.java
+++ src/main/java/dev/jtphan/movies/ReviewRepository.java
@@ -1,6 +1,10 @@
 package dev.jtphan.movies;
 
 import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +13,6 @@ import java.util.Optional;
 
 @Repository
 public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
 
 }
