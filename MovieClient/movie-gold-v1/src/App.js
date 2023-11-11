2e054ec35a48d0beea66cea9d3a9b775e2e5136e
Jonathan Phan <jonathan.tan.phan@gmail.com>
2023-11-11 12:55:01 -0700

    Create Layout and Home components and initiate Routing

--- MovieClient/movie-gold-v1/src/App.js
+++ MovieClient/movie-gold-v1/src/App.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import api from './api/axiosConfig';
 import {useState, useEffect} from 'react';
+import Layout from './components/Layout';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
 
@@ -27,7 +30,12 @@ function App() {
 
   return (
     <div className="App">
+        <Routes>
+            <Route path="/" element={<Layout/>}>
+                <Route path ="/" element ={<Home/>} ></Route>
 
+            </Route>
+        </Routes>
     </div>
   );
 }
