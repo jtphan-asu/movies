92a2839977d2cf0c14ad249f131c7f1b8f3e088b
Jonathan Phan <jonathan.tan.phan@gmail.com>
2023-11-21 01:01:12 -0700

    Create ReviewForm.js and edit Trailer.js

--- MovieClient/movie-gold-v1/src/components/trailer/Trailer.js
+++ MovieClient/movie-gold-v1/src/components/trailer/Trailer.js
@@ -6,12 +6,12 @@ import React from 'react';
 
 const Trailer = () => {
 
-    const params = useParams();
-    const key = params.ytTrailerId;
+    let params = useParams();
+    let key = params.ytTrailerId;
 
     return(
         <div className="react-player-container">
-              {(key!=null)?<ReactPlayer controls="true" playing={true} url ={`https://www.youtube.com/watch?v=${key}`}
+              {(key!=null)?<ReactPlayer controls="true" playing={true} url ={'https://www.youtube.com/watch?v=${key}'}
               width = '100%' height='100%' />:null}
             </div>
     )
